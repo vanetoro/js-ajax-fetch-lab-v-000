bc63c896ba98d9bf9400b678797350320472c80f
Vanessa Toro <vanetoro84@gmail.com>
2018-08-11 19:33:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function showResults(json) {
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   fetch(`https://api.github.com/repos/${repo}/forks`, {
-    method: 'POST',
+    method: 'post',
     headers: {
       Authorization: `token ${getToken()}`
     }
